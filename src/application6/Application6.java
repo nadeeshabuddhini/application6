a5a8bedc5c8ee36f4b0130608bd7115a2213f1c4
Nadeesha Buddhini <hknadeesha1995buddhi@gmail.com>
2019-02-26 22:45:11 +0530

    application6
    application6

---
+++ src/application6/Application6.java
@@ -0,0 +1,15 @@
+
+package application6;
+
+public class Application6 {
+
+   
+    public static void main(String[] args) {
+        int counter=0;
+       while (counter<10){
+           System.out.println(counter);
+           counter++;
+       }
+    }
+    
+}
